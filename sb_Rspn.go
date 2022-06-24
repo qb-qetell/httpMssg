82f63bbef36f0fbe7ad16bafe1ae8a776ec2ede4
Brian Ibrahim Qamardeen <brian@qeetell.eon>
2022-06-24 12:05:42 +0100

    Progress

--- sb_Rspn.go
+++ sb_Rspn.go
@@ -2,7 +2,6 @@ package httpMssg
 
 import (
 	"net/http"
-	"strconv"
 )
 
 type Rspn struct {
@@ -12,6 +11,6 @@ type Rspn struct {
 	func rspn_estb (rspn *http.Response, mssg []byte) (*Rspn) {
 		return &Rspn {Rspn: rspn, mssg: mssg}	
 	}
-	func (i *Rspn) ExtrRspn () (string, []byte) {
-		return strconv.Itoa (i.Rspn.StatusCode), i.mssg
+	func (i *Rspn) ExtrRspn () ([]byte) {
+		return i.mssg
 	}
--- sx_test.go
+++ sx_test.go
@@ -11,19 +11,19 @@ func Test_ (t *testing.T) {
 	_ka00.SettMssg ([]byte ("hello world!"))
 	_ka00.SettRcpn ("https://hello-world.free.beeceptor.com/my/api/path")
 	_ka00.SettCmmnMthd ("POST")
-	_ka00.SettHdrr ("header1", "12345678")
-	_ka00.SettHdrr ("header2", "12345678")
-	_ka00.SettHdrr ("header3", "12345678")
-	_ka00.SettHdrr ("header4", "12345678")
+	_ka00.SettHdrr ("hdrr01", "a:123456")
+	_ka00.SettHdrr ("hdrr02", "b:123456")
+	_ka00.SettHdrr ("hdrr03", "c:123456")
+	_ka00.SettHdrr ("hdrr04", "d:123456")
 	_kb00, _kc00 := _ka00.Send (time.Second * 4)
 	
 	if _kb00 != nil {
 		_kd00 := fmt.Sprintf ("Errr: %s", _kb00.Error ())
 		fmt.Println (_kd00)
 	} else {
-		_ke00, _kf00 := _kc00.ExtrRspn ()
-		_kg00 := fmt.Sprintf ("Code: %s", _ke00)
-		_kh00 := fmt.Sprintf ("Rspn: %s", _kf00)
+		_ke00 := _kc00.ExtrRspn ()
+		_kg00 := fmt.Sprintf ("Code: %d", _kc00.Rspn.StatusCode)
+		_kh00 := fmt.Sprintf ("Rspn: %s", _ke00)
 		fmt.Println (_kg00)
 		fmt.Println (_kh00)
 	}
